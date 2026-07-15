24705522157b50f144bb60f7945469f0380d3e7c
Braxton C <braxton.a.chatman@gmail.com>
2026-07-15 12:54:04 -0700

    Add api tests for comparison

--- tests/test_food_api.py
+++ tests/test_food_api.py
@@ -41,3 +41,45 @@ def test_food(client):
     body = response.get_json()
 
     assert body["success"] is True
+
+
+def test_comparison_requires_food1(client):
+    response = client.get("/api/comparison?food2=apple")
+
+    assert response.status_code == 400
+
+def test_comparison_requires_food2(client):
+    response = client.get("/api/comparison?food1=banana")
+
+    assert response.status_code == 400
+
+def test_comparison_requires_inputs(client):
+    response = client.get("/api/comparison")
+
+    assert response.status_code == 400
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "/api/comparison?food1=&food2=apple",
+        "/api/comparison?food1=banana&food2=",
+    ]
+)
+def test_comparison_empty_values(client, url):
+
+    response = client.get(url)
+
+    assert response.status_code == 400
+
+def test_comparison_success(client):
+
+    response = client.get(
+        "/api/comparison?food1=chicken&food2=apple"
+    )
+
+    assert response.status_code == 200
+
+    body = response.get_json()
+
+    assert body["success"] is True
+    assert "comparison_insights" in body["data"]
